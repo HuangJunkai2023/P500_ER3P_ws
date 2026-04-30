f8b4d80a437f919ec99991fdb9e973f822015f07
HuangJK <442827947@qq.com>
2026-04-30 11:30:18 +0800

    Set realtime UArm gripper zero as open

--- src/xCoreSDK_cpp-v0.7.1/example/uarm_er3pro_rt.cpp
+++ src/xCoreSDK_cpp-v0.7.1/example/uarm_er3pro_rt.cpp
@@ -93,8 +93,8 @@ struct Config {
   std::array<double, 7> max_speed_deg = {120.0, 120.0, 120.0, 160.0, 160.0, 160.0, 160.0};
   std::array<double, 7> max_accel_deg = {600.0, 600.0, 600.0, 800.0, 800.0, 800.0, 800.0};
   std::array<double, 7> rt_collision_thresholds = {75.0, 75.0, 60.0, 45.0, 30.0, 30.0, 20.0};
-  double gripper_open_deg = 270.0;
-  double gripper_close_deg = 0.0;
+  double gripper_open_deg = 0.0;
+  double gripper_close_deg = 270.0;
   bool enable_gripper = true;
   GripperBackend gripper_backend = GripperBackend::Rs485Epg;
   double gripper_threshold = 0.5;
